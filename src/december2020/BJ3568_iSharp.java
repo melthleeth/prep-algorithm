40d3677d51696f26a080ebf85a4545b55b75a311
Hyejin Lee <hichjay@gmail.com>
2020-12-22 21:50:11 +0900

    Silver 5 / 구현, 문자열

--- src/december2020/BJ3568_iSharp.java
+++ src/december2020/BJ3568_iSharp.java
@@ -3,36 +3,62 @@ package december2020;
 import java.util.*;
 import java.io.*;
 
+//2020.12.22
+// 문제를 잘 읽자.. 그냥 반대로 뒤집기만 하면 되는 거였다..
+
 public class BJ3568_iSharp {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String[] str = br.readLine().split(" ");
-
+        
         StringBuilder[] sb = new StringBuilder[str.length + 1];
-        for (int i = 0; i < str.length + 1; i++)
+        for (int i = 0; i <= str.length; i++)
             sb[i] = new StringBuilder();
-
-        for (int i = 1; i < str.length - 1; i++) {
-            sb[i].append(str[0]);
-
-            // 순서: & [] *
-            Stack<String> s = new Stack<>();
-            String varName = "";
-            for (int j = 0; j < str[i].length(); j++){
-                char c = str[i].charAt(j);
-                if (c == '*' || c == '[' || c == '&'){
-
-                } else varName = str[i].substring(0, str[i].length() - 1);
-            }
-
+        
+        for (int i = 1; i < str.length; i++) {
+        	sb[i].append(str[0]);
+            
+        	int idx = getIdx(str[i]);
+        	String varName = str[i].substring(0, idx);
+            
+            sb[i].append(getType(idx, str[i]));
             sb[i].append(" " + varName + ";\n");
         }
 
-        for (StringBuilder sbr : sb)
-            bw.write(sbr.toString());
+        for (int i = 1; i < str.length; i++)
+            bw.write(sb[i].toString());
 
         br.close();
         bw.close();
+    }
+    
+    public static int getIdx(String str) {
+    	int idx = str.length() - 1;
+    	for (int j = 0; j < str.length(); j++) {
+        	char c = str.charAt(j);
+        	if ((c == '*' || c == '[' || c == '&')) return j;
+        }
+    	return idx;
+    }
+    
+    public static String getType(int idx, String str) {
+    	StringBuilder result = new StringBuilder();
+    	for (int j = str.length() - 1; j >= idx; j--) {
+    		char c = str.charAt(j);
+    		
+        	switch(c) {
+            case '&':
+            case '*':
+            	result.append(c);
+            	break;
+            case ']':
+            	result.append("[]");
+            	j--;
+            	break;
+            }
+    	}
+    	
+    	return result.toString();
     }
 }
