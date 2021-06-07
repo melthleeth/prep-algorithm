a32338148fbe026db5f7efb8a3ed121cde6cc587
Hyejin Lee <hichjay@gmail.com>
2021-06-07 23:58:30 +0900

    DP | Gold 5

--- src/june2021/Bj12865_G5_평범한배낭.java
+++ src/june2021/Bj12865_G5_평범한배낭.java
@@ -3,9 +3,12 @@ package june2021;
 import java.io.*;
 import java.util.*;
 
+// 틀린 이유: dp[i - 1][j - W[i]] + V[i] 에서 dp[i][j - W[i]] + V[i] 라고 해서
+
 public class Bj12865_G5_평범한배낭 {
 	static int N, K, maxValue;
-	static int[][] product;
+	static int[] W, V;
+	static int[][] dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,15 +17,29 @@ public class Bj12865_G5_평범한배낭 {
 
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		product = new int[N + 1][2];
+		W = new int[N + 1];
+		V = new int[N + 1];
+		dp = new int[N + 1][K + 1];
 		
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			product[i][0] = Integer.parseInt(st.nextToken());
-			product[i][1] = Integer.parseInt(st.nextToken());
+			W[i] = Integer.parseInt(st.nextToken());
+			V[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		bw.write(String.valueOf(maxValue));
+		for (int j = 1; j <= K; j++) {
+			for (int i = 1; i <= N; i++) {
+				if (j - W[i] >= 0)
+					dp[i][j] = Math.max(dp[i - 1][j - W[i]] + V[i], dp[i - 1][j]);
+				else
+					dp[i][j] = dp[i - 1][j];
+			} 
+		}
+
+//		for (int[] row : dp)
+//			System.out.println(Arrays.toString(row));
+		
+		bw.write(String.valueOf(dp[N][K]));
 		br.close();
 		bw.close();
 	}
