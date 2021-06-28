e1e8b8cda2b30a296d3b34a84dac4b0656e5ece5
melthleeth <hichjay@gmail.com>
2021-06-28 16:47:44 +0900

    DFS | Gold 4

--- src/june2021/BJ9466_G4_텀프로젝트.java
+++ src/june2021/BJ9466_G4_텀프로젝트.java
@@ -4,9 +4,10 @@ import java.io.*;
 import java.util.*;
 
 public class BJ9466_G4_텀프로젝트 {
-	static int T, n, sum;
+	static int T, n, cnt;
+	static final int SIZE = 100010;
 	static int[] arr;
-	static boolean[] visited;
+	static boolean[] visited, finished;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,36 +18,36 @@ public class BJ9466_G4_텀프로젝트 {
 		
 		while(T-- > 0) {
 			n = Integer.parseInt(br.readLine());
-			sum = n;
-			arr = new int[n + 1];
-			visited = new boolean[n + 1];
+			cnt = 0;
+			arr = new int[SIZE];
+			visited = new boolean[SIZE];
+			finished = new boolean[SIZE];
 			st = new StringTokenizer(br.readLine());
-			
-			for (int i = 0; i < n; i++)
+
+			for (int i = 1; i <= n; i++)
 				arr[i] = Integer.parseInt(st.nextToken());
-			
-			
-			
-			for (int i = 0; i < n; i++)
-				if (!visited[i]) explore(i, i, 0);
-			
-			bw.write(sum + "\n");
+
+			for (int i = 1; i <= n; i++)
+				if (!visited[i]) explore(i);
+
+
+			bw.write((n - cnt) + "\n");
 		}
 		br.close();
 		bw.close();
 	}
 	
-	public static void explore(int start, int n, int cnt) {
-		if (visited[n]) {
-			if (n == start)
-				sum -= cnt;
-			
-			return;
-		}
-		
+	public static void explore(int n) {
 		visited[n] = true;
-		explore(start, arr[n], cnt + 1);
-//		visited[n] = false;
+
+		int next = arr[n];
+
+		if (!visited[next]) explore(next);
+		else if (!finished[next]) {
+			for (int i = next; i != n; i = arr[i]) cnt++;
+			cnt++;
+		}
+		finished[n] = true;
 	}
 
 }
