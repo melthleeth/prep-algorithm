373d3643e3fabf947611981f33f5de2f35270464
Hyejin Lee <hichjay@gmail.com>
2021-07-05 23:57:50 +0900

    S1 문제 풀이중

---
+++ src/july2021/BJ14496_S1_그대그머가되어.java
@@ -0,0 +1,17 @@
+package july2021;
+import java.io.*;
+import java.util.*;
+
+public class BJ14496_S1_그대그머가되어 {
+    static int a, b;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+
+        br.close();
+        bw.close();
+    }
+}
