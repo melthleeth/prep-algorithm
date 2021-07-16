9d810a069759b1d9acae29359b62698af704d19e
Hyejin Lee <hichjay@gmail.com>
2021-07-16 11:33:40 +0900

    백트래킹, 브루트포스 | S1

--- src/july2021/BJ16198_S1_에너지모으기.java
+++ src/july2021/BJ16198_S1_에너지모으기.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class BJ16198_S1_에너지모으기 {
     static int N;
-    static long maxValue, ans;
+    static long ans;
     static int[] arr;
     static boolean[] visited;
 
@@ -16,46 +16,41 @@ public class BJ16198_S1_에너지모으기 {
 
         N = Integer.parseInt(br.readLine());
         arr = new int[N];
+        visited = new boolean[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++)
             arr[i] = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i < N - 1; i++) {
-            visited = new boolean[N];
-            visited[i] = true;
-            solve(i, 0);
-            ans = Math.max(ans, maxValue);
-        }
+        solve(0, 0);
 
         bw.write(String.valueOf(ans));
         br.close();
         bw.close();
     }
 
-    public static void solve(int n, long sum) {
-        int left = n, right = n;
-
-        while(left-- > 0) {
-            if (!visited[left]) break;
-        }
-
-        while(right++ < N - 1) {
-            if (!visited[right]) break;
-        }
-        System.out.println("left, right = " + arr[left] + ", " + arr[right]);
-        if (left == 0 && right == N - 1) {
-            System.out.println("끝: " + (sum + arr[left] * arr[right]));
-            maxValue = Math.max(maxValue, sum + arr[left] * arr[right]);
+    // for문 안에 left, right 찾기를 해야함
+    // 백트래킹 form으로 가기
+    public static void solve(int cnt, long sum) {
+        if (cnt == N - 2) {
+            ans = Math.max(sum, ans);
             return;
         }
 
-        visited[n] = true;
-        if (left == 0) solve(right, (sum + arr[left] * arr[right]));
-        else if (right == N - 1) solve(left, (sum + arr[left] * arr[right]));
-        else {
-            solve(left, (sum + arr[left] * arr[right]));
-            solve(right, (sum + arr[left] * arr[right]));
+        for (int i = 1; i < N-1; i++) {
+            if (visited[i]) continue;
+
+            int left = i, right = i;
+            while(left-- > 0) {
+                if (!visited[left]) break;
+            }
+
+            while(right++ < N - 1) {
+                if (!visited[right]) break;
+            }
+
+            visited[i] = true;
+            solve(cnt + 1, sum + (arr[left] * arr[right]));
+            visited[i] = false;
         }
-        visited[n] = false;
     }
 }
