bdf97f1691830f003fa702498c23c3be5580d63e
Hyejin Lee <hichjay@gmail.com>
2021-07-20 17:26:10 +0900

    오늘 풀어야될 문제들

---
+++ src/july2021/BJ1197_G4_최소스패닝트리.java
@@ -0,0 +1,13 @@
+package july2021;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ1197_G4_최소스패닝트리 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    }
+}
---
+++ src/july2021/BJ15684_G4_사다리조작.java
@@ -0,0 +1,28 @@
+package july2021;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ15684_G4_사다리조작 {
+    static int N, M, H;
+    static int[][] map;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+        map = new int[N + 1][H + 1];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            map[x][y] = 1;
+            map[x][y + 1] = 1;
+        }
+    }
+}
