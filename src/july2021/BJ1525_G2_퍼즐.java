a7f0c66cfe27bbd6ad4685d94ad864f9d7290057
melthleeth <hichjay@gmail.com>
2021-07-31 23:47:00 +0900

    BFS | Gold 2

--- src/july2021/BJ1525_G2_퍼즐.java
+++ src/july2021/BJ1525_G2_퍼즐.java
@@ -3,25 +3,25 @@ package july2021;
 import java.io.*;
 import java.util.*;
 
+// hint: 2차원 배열로 저장하지 말고 linear한 값으로 저장하기
 public class BJ1525_G2_퍼즐 {
-    static int ans;
+    static int ans = Integer.MAX_VALUE;
     static int[] px = new int[]{1, 0, -1, 0};
     static int[] py = new int[]{0, 1, 0, -1};
-    static int[][] map = new int[4][4];
-    static Queue<Coord> q = new LinkedList<>();
+    static StringBuilder init = new StringBuilder();
+    static Map<String, Integer> visited = new HashMap<>();
+    static final String ANS = "123456780";
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        for (int i = 1; i <= 3; i++) {
+        for (int i = 0; i < 3; i++) {
             st = new StringTokenizer(br.readLine());
 
-            for (int j = 1; j <= 3; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == 0) q.offer(new Coord(i, j, 0));
-            }
+            for (int j = 0; j < 3; j++)
+                init.append(st.nextToken());
         }
         solve();
 
@@ -31,41 +31,43 @@ public class BJ1525_G2_퍼즐 {
     }
 
     public static void solve() {
-        while(!q.isEmpty()) {
-            int x = q.peek().x;
-            int y = q.peek().y;
-            int cnt = q.peek().cnt;
+        Queue<String> q = new LinkedList<>();
+        visited.put(init.toString(), 0);
+        q.offer(init.toString());
 
+        while (!q.isEmpty()) {
+            String str = q.poll();
+
+            if (str.equals(ANS)) break;
+
+            int pos = str.indexOf("0");
+            int x = pos / 3;
+            int y = pos % 3;
             for (int d = 0; d < 4; d++) {
                 int nx = x + px[d];
                 int ny = y + py[d];
+
                 if (!isIn(nx, ny)) continue;
+                int idx = nx * 3 + ny;
+                char target = str.charAt(idx);
+                String nextStr = str;
+                nextStr = nextStr.replace('0', target);
+                nextStr = replace(nextStr, idx, '0');
+                if (visited.get(nextStr) != null) continue;
+                visited.put(nextStr, visited.get(str) + 1);
+                q.offer(nextStr);
             }
         }
+        if (visited.get(ANS) == null) ans = -1;
+        else ans = visited.get(ANS);
+    }
+
+    public static String replace(String str, int idx, char c) {
+        return str.substring(0, idx) + c + str.substring(idx + 1);
     }
 
     public static boolean isIn(int x, int y) {
-        if (x < 1 || x > 3 || y < 1 || y > 3) return false;
+        if (x < 0 || x >= 3 || y < 0 || y >= 3) return false;
         return true;
-    }
-
-    public static boolean isMatch() {
-        for (int i = 1; i <= 3; i++)
-            for (int j = 1; j <= 3; j++)
-                if (map[i][j] != (i - 1) * 3 + j) return false;
-
-        return true;
-    }
-
-    static class Coord {
-        int x;
-        int y;
-        int cnt;
-
-        public Coord(int x, int y, int cnt) {
-            this.x = x;
-            this.y = y;
-            this.cnt = cnt;
-        }
     }
 }
