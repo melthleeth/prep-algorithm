a83f37e064aa54259554ccc62609a62a3ac0327f
Hyejin Lee <hichjay@gmail.com>
2021-10-17 00:10:35 +0900

    DP | Silver 5

--- src/october2021/BJ6245_S5_CowSolitaire.java
+++ src/october2021/BJ6245_S5_CowSolitaire.java
@@ -12,31 +12,38 @@ public class BJ6245_S5_CowSolitaire {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
         N = Integer.parseInt(br.readLine());
-        arr = new int[N + 1][N + 1];
-        sum = new int[N + 1][N + 1];
+        arr = new int[N][N];
+        sum = new int[N][N];
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
+            for (int j = 0; j < N; j++) {
                 char c = st.nextToken().charAt(0);
 
-                if (c == 'T') arr[i][j] = 10;
+                if (c == 'A') arr[i][j] = 1;
+                else if (c == 'T') arr[i][j] = 10;
                 else if (c == 'J') arr[i][j] = 11;
                 else if (c == 'Q') arr[i][j] = 12;
                 else if (c == 'K') arr[i][j] = 13;
                 else arr[i][j] = c - '0';
-
-                arr[i][j] += arr[i][j - 1];
             }
         }
 
-        for (int i = N; i > 0; i--) {
-            for (int j = 1; j <= N; j++) {
+        for (int i = N - 2; i >= 0; i--)
+            arr[i][0] += arr[i + 1][0];
+        for (int j = 1; j < N; j++)
+            arr[N - 1][j] += arr[N - 1][j - 1];
 
+        for (int i = N - 2; i >= 0; i--) {
+            for (int j = 1; j < N; j++) {
+                arr[i][j] += Math.max(arr[i + 1][j], arr[i][j - 1]);
             }
         }
 
-        bw.write(String.valueOf(sum[0][N - 1]));
+//        for (int[] col : arr)
+//            System.out.println(Arrays.toString(col));
+
+        bw.write(String.valueOf(arr[0][N - 1]));
         br.close();
         bw.close();
     }
