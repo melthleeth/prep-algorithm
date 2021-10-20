a8ffdfb13d5fa7697ae6a657a94ce5f62e96c85e
Hyejin Lee <hichjay@gmail.com>
2021-10-21 04:45:37 +0900

    문자열, 정규 표현식 | Gold 5

---
+++ src/october2021/BJ2671_G5_잠수함식별.java
@@ -0,0 +1,28 @@
+package october2021;
+
+import java.io.*;
+
+public class BJ2671_G5_잠수함식별 {
+
+    public static void main(String[] args) throws IOException {
+        String ans = "SUBMARINE";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = br.readLine();
+        String str = input;
+
+        while (true) {
+            String next = str.replaceFirst("(100+1+)$", "");
+            if (next.equals(str))
+                next = next.replaceFirst("01$", "");
+            if (next.equals(str)) break;
+            str = next;
+        }
+
+        if (str.equals(input) || !str.isEmpty()) ans = "NOISE";
+
+        bw.write(ans);
+        br.close();
+        bw.close();
+    }
+}
