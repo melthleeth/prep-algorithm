e95bfd8233983d5b211e894d27f0cf50e13dfd7e
Hyejin Lee <hichjay@gmail.com>
2021-11-18 16:54:37 +0900

    점프 | 실버2 | DP (ing)

--- src/november2021/BJ1890_S2_점프.java
+++ src/november2021/BJ1890_S2_점프.java
@@ -5,8 +5,9 @@ import java.util.*;
 import java.awt.*;
 
 public class BJ1890_S2_점프 {
-    static int N, ans;
-    static int[][] arr, cnt;
+    static int N;
+    static int[][] arr;
+    static long[][] cnt;
     static int[] px = new int[]{1, 0};
     static int[] py = new int[]{0, 1};
     static Queue<Point> q = new LinkedList<>();
@@ -17,7 +18,7 @@ public class BJ1890_S2_점프 {
         StringTokenizer st;
         N = Integer.parseInt(br.readLine());
         arr = new int[N][N];
-        cnt = new int[N][N];
+        cnt = new long[N][N];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -27,12 +28,37 @@ public class BJ1890_S2_점프 {
         }
         q.offer(new Point(0, 0));
 
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
+            int x = q.peek().x;
+            int y = q.peek().y;
+            q.poll();
 
+            if (arr[x][y] == 0) continue;
+
+            for (int d = 0; d < 2; d++) {
+                int nx = x + px[d] * arr[x][y];
+                int ny = y + py[d] * arr[x][y];
+
+                if (!isIn(nx, ny)) continue;
+                if (cnt[nx][ny] == 0)
+                    cnt[nx][ny]++;
+                else
+                    cnt[nx][ny] += cnt[x][y];
+                if (cnt[nx][ny] == 1)
+                    q.offer(new Point(nx, ny));
+
+            }
         }
+        for (long[] col : cnt)
+            System.out.println(Arrays.toString(col));
 
-        bw.write(String.valueOf(ans));
+        bw.write(String.valueOf(cnt[N - 1][N - 1]));
         br.close();
         bw.close();
+    }
+
+    public static boolean isIn(int x, int y) {
+        if (x < 0 || x >= N || y < 0 || y >= N) return false;
+        return true;
     }
 }
