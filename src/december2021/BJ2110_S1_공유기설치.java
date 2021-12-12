25bdb3a67624005418fba0d9eff4837c14b4eabb
Hyejin Lee <hichjay@gmail.com>
2021-12-12 23:53:27 +0900

    어떻게 푸는거지;;

---
+++ src/december2021/BJ2110_S1_공유기설치.java
@@ -0,0 +1,41 @@
+package december2021;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ2110_S1_공유기설치 {
+    static int N, C, ans;
+    static int[] house;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        house = new int[N];
+
+        for (int i = 0; i < N; i++)
+            house[i] = Integer.parseInt(br.readLine());
+
+        Arrays.sort(house);
+
+        int start = house[0];
+        int end = house[N - 1];
+        int startIdx = 0;
+        int endIdx = N - 1;
+        ans = end - start;
+        C -= 2;
+
+        while (C-- > 0) {
+            int midIdx = (startIdx + endIdx) / 2;
+            int mid = (start + end) / 2;
+        }
+
+        bw.write(String.valueOf(ans));
+        br.close();
+        bw.close();
+    }
+}
