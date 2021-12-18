30a5f90c6ce9ddbf6bac9bd0e77ce2304f355ca8
Hyejin Lee <hichjay@gmail.com>
2021-12-19 00:15:50 +0900

    두 용액 | 골드5 | 정렬, 투포인터

--- src/december2021/BJ2470_G5_두용액.java
+++ src/december2021/BJ2470_G5_두용액.java
@@ -4,15 +4,51 @@ import java.io.*;
 import java.util.*;
 
 public class BJ2470_G5_두용액 {
-    static int ans;
+    static int N, ans = Integer.MAX_VALUE, startIdx, endIdx;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
 
-        bw.write(String.valueOf(ans));
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        int start = 0;
+        int end = N - 1;
+        endIdx = N - 1;
+
+        while (start < end) {
+            int sum = arr[start] + arr[end];
+
+            if (sum > 0) {
+                if (sum < Math.abs(ans)) {
+                    startIdx = start;
+                    endIdx = end;
+                    ans = sum;
+                }
+                end--;
+            } else if (sum < 0) {
+                if (-sum < Math.abs(ans)) {
+                    startIdx = start;
+                    endIdx = end;
+                    ans = sum;
+                }
+                start++;
+            } else {
+                startIdx = start;
+                endIdx = end;
+                break;
+            }
+        }
+
+        bw.write(arr[startIdx] + " " + arr[endIdx]);
         br.close();
         bw.close();
     }
