32cfcbcbe9f4ac6445152f51e624b5f34a81b77c
melthleeth <hichjay@gmail.com>
2021-12-20 12:32:07 +0900

    줄세우기 | 골드3 | 위상정렬 - DFS와 indegree를 이용한 queue 풀이가 있는데 indegree가 더 끌려서 indegree로.. - 1766 골드2 문제집과 코드 똑같음

--- src/december2021/BJ2252_G3_줄세우기.java
+++ src/december2021/BJ2252_G3_줄세우기.java
@@ -5,7 +5,9 @@ import java.util.*;
 
 public class BJ2252_G3_줄세우기 {
     static int N, M;
-    static int[] root;
+    static int[] indegree;
+    static TreeSet<Integer>[] set;
+    static PriorityQueue<Integer> pq = new PriorityQueue<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,15 +17,33 @@ public class BJ2252_G3_줄세우기 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        root = new int[N + 1];
+        indegree = new int[N + 1];
+        set = new TreeSet[N + 1];
+        for (int i = 0; i <= N; i++)
+            set[i] = new TreeSet<>();
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int front = Integer.parseInt(st.nextToken());
-            int back = Integer.parseInt(st.nextToken());
-
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            set[from].add(to);
+            indegree[to]++;
         }
 
-        bw.write("");
+        for (int i = 1; i <= N; i++)
+            if (indegree[i] == 0)
+                pq.offer(i);
+
+        while (!pq.isEmpty()) {
+            int from = pq.poll();
+            bw.write(from + " ");
+
+            for (int to : set[from]) {
+                indegree[to]--;
+                if (indegree[to] == 0) pq.offer(to);
+            }
+        }
+
         br.close();
         bw.close();
     }
